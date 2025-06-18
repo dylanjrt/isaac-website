c15ea33cb74bb9f6543fc33773fe68a0d78c40fd
Dylan Rochon-Terry <dylanjrt@gmail.com>
2025-06-18 17:08:39 -0400

    Add a basic contact page

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,7 +1,31 @@
-export default function Contact() {
+import { getContactItems } from "@/sanity/queries";
+import { ContactItem } from "@/types/sanity.types";
+import Link from "next/link";
+
+export default async function Contact() {
+  const contactItems: ContactItem[] = await getContactItems();
+
   return (
-    <div className="flex flex-col items-center justify-center h-screen">
-      <h1 className="text-4xl font-bold">Contact</h1>
+    <div className="flex w-full max-w-4xl flex-col gap-8 p-12">
+      <div className="flex flex-col gap-6">
+        {contactItems.map((item) => (
+          <div key={item._id}>
+            <span className="font-bold">{item.name}:</span>{" "}
+            {item.link ? (
+              <Link
+                href={item.link}
+                className="underline"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                {item.displayedAs || item.link}
+              </Link>
+            ) : (
+              <span>{item.displayedAs}</span>
+            )}
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
