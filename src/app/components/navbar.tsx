e2ff69780510f8b3677505ce3f261cd0707a0452
Dylan Rochon-Terry <dylanjrt@gmail.com>
2025-07-17 14:08:46 -0400

    Add logo image to navbar and adjust layout for improved aesthetics

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -2,6 +2,7 @@
 "use client";
 
 import Link from "next/link";
+import Image from "next/image";
 import { usePathname } from "next/navigation";
 import { Arizonia } from "next/font/google";
 
@@ -21,12 +22,20 @@ export default function Navbar() {
   const pathname = usePathname();
 
   return (
-    <nav className="bg-background text-primary h-full p-8 px-24">
-      <div className="flex items-center justify-between">
+    <nav className="bg-background text-primary h-24 p-8 px-24">
+      <div className="flex h-full items-center justify-between">
         <div
           className={`flex items-center pr-12 text-4xl whitespace-nowrap ${arizonia.className}`}
         >
-          <Link href="/">ISAAC TEA</Link>
+          <Link href="/">
+            <Image
+              src="/logo.png"
+              alt="ISAAC TEA"
+              width={300}
+              height={40}
+              className="h-full w-auto"
+            />
+          </Link>
         </div>
         <div className="flex items-center justify-end gap-16 text-lg font-bold">
           {navLinks.map((link) => (
